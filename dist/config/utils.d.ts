8528f05c5007ab23100a80c98a7cb8e2327fe972
Eagle <sahilkhan8294799@gmail.com>
2025-11-14 12:45:59 +0530

    Remove dist from git

--- dist/config/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getExpiryDate: (days: number) => number;
-export declare const isExpired: (expiryTimestamp: number) => boolean;
-export declare function convertBigIntToString<T = any>(obj: T): T;
-//# sourceMappingURL=utils.d.ts.map
--- dist/middlewares/auth.middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { IDecodedToken } from '../services/jwt.service.js';
-export interface AuthenticatedRequest extends Request {
-    user?: IDecodedToken;
-}
-export declare function authenticateJWT(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void | Response<any, Record<string, any>>>;
-//# sourceMappingURL=auth.middleware.d.ts.map
